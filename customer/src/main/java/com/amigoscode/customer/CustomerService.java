321fb1712e3f0745f8b215b211eccb2e4ca8d1c0
Aleksandr Berestov <ber_alex@mail.ru>
2022-07-07 18:47:38 +0400

    docker customer

---
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -0,0 +1,21 @@
+package com.amigoscode.customer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public void register(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .build();
+
+        customerRepository.save(customer);
+    }
+}
